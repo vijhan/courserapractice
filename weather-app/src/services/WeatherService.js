027caef82395d54217b41114fd2a840324b5b96a
vijhan <97991833+vijhan@users.noreply.github.com>
2024-08-10 10:25:35 -0500

    fixed temperature

--- weather-app/src/services/WeatherService.js
+++ weather-app/src/services/WeatherService.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 export async function getWeatherData(location) {
-    return await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`);
+    return await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=imperial`);
     
 }
